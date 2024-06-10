3d4ee2570aaad70b6b2996ed5e522dd63d7d5267
bekah <bekahritter@gmail.com>
2024-06-10 10:22:52 -0700

    fixed post and update

--- server/index.js
+++ server/index.js
@@ -38,8 +38,37 @@ app.get("/api/reservations", async (req, res, next) => {
     next(ex);
   }
 });
-// POST /api/customers/:id/reservations - payload: an object which has a valid restaurant_id, date, and party_count.
-// returns the created reservation with a status code of 201
+
+app.post("/api/customers/:id/reservations", async (req, res, next) => {
+  try {
+    res.status(201).send(
+      await createReservation({
+        date: req.body.date,
+        party_count: req.body.party_count,
+        restaurant_id: req.body.restaurant_id,
+        customer_id: req.params.id,
+      })
+    );
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.delete(
+  "/api/customers/:customer_id/reservations/:id",
+  async (req, res, next) => {
+    try {
+      await destroyReservation({
+        customer_id: req.params.customer_id,
+        id: req.params.id,
+      });
+      res.sendStatus(204);
+    } catch (ex) {
+      next(ex);
+    }
+  }
+);
+
 // DELETE /api/customers/:customer_id/reservations/:id
 
 const init = async () => {
